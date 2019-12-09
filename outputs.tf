1e69fa0b44cfbbbeff0d63082286cee975d10f63
Nicolas Martinez <nmartinez@corpit-consulting.com>
2019-12-09 14:04:15 -0300

    fix output.tf

--- outputs.tf
+++ outputs.tf
@@ -60,5 +60,5 @@ output "security_groups" {
 
 output "arn" {
   description = "The ARN of the load balancer"
-  value       = "${element(concat(aws_lb.application.*.arn, aws_lb.application_no_logs.*.arn, aws_lb.network_no_logs.arn, aws_lb.network.*.arn), 0)}"
+  value       = "${slice(concat(aws_lb.application.*.arn, aws_lb.application_no_logs.*.arn, aws_lb.network_no_logs.arn, aws_lb.network.*.arn), 0)}"
 }
