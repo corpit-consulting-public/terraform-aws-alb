63f27ddc3be972e8e3659b3f05fe41680c33ba6b
Nicolas Martinez <nmartinez@corpit-consulting.com>
2019-12-09 11:32:04 -0300

    fix output ELB, support NLB

--- outputs.tf
+++ outputs.tf
@@ -1,6 +1,6 @@
 output "dns_name" {
   description = "The DNS name of the load balancer."
-  value       = "${element(concat(aws_lb.application.*.dns_name, aws_lb.application_no_logs.*.dns_name), 0)}"
+  value       = "${element(concat(aws_lb.application.*.dns_name, aws_lb.application_no_logs.*.dns_name, aws_lb.network_no_logs.dns_name, aws_lb.network.*.dns_name), 0)}"
 }
 
 output "http_tcp_listener_arns" {
@@ -25,17 +25,17 @@ output "https_listener_ids" {
 
 output "load_balancer_arn_suffix" {
   description = "ARN suffix of our load balancer - can be used with CloudWatch."
-  value       = "${element(concat(aws_lb.application.*.arn_suffix, aws_lb.application_no_logs.*.arn_suffix), 0)}"
+  value       = "${element(concat(aws_lb.application.*.arn_suffix, aws_lb.application_no_logs.*.arn_suffix, aws_lb.network_no_logs.arn_suffix, aws_lb.network.*.arn_suffix), 0)}"
 }
 
 output "load_balancer_id" {
   description = "The ID and ARN of the load balancer we created."
-  value       = "${element(concat(aws_lb.application.*.id, aws_lb.application_no_logs.*.id), 0)}"
+  value       = "${element(concat(aws_lb.application.*.id, aws_lb.application_no_logs.*.id, aws_lb.network_no_logs.id, aws_lb.network.*.id), 0)}"
 }
 
 output "load_balancer_zone_id" {
   description = "The zone_id of the load balancer to assist with creating DNS records."
-  value       = "${element(concat(aws_lb.application.*.zone_id, aws_lb.application_no_logs.*.zone_id), 0)}"
+  value       = "${element(concat(aws_lb.application.*.zone_id, aws_lb.application_no_logs.*.zone_id, aws_lb.network_no_logs.zone_id, aws_lb.network.*.zone_id), 0)}"
 }
 
 output "target_group_arns" {
@@ -55,10 +55,10 @@ output "target_group_names" {
 
 output "security_groups" {
   description = "List of Security Groups IDs"
-  value       = "${aws_lb.application_no_logs.*.security_groups}"
+  value       = "${element(concataws_lb.application_no_logs.*.security_groups, aws_lb.network_no_logs.*.security_groups), 0)}"
 }
 
 output "arn" {
   description = "The ARN of the load balancer"
-  value       = "${element(concat(aws_lb.application.*.arn, aws_lb.application_no_logs.*.arn), 0)}"
+  value       = "${element(concat(aws_lb.application.*.arn, aws_lb.application_no_logs.*.arn, aws_lb.network_no_logs.arn, aws_lb.network.*.arn), 0)}"
 }
