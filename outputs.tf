e45a783fb434ae966f5bef2e263ee7aa2b66f845
Nicolas Martinez <nmartinez@corpit-consulting.com>
2019-12-09 14:29:56 -0300

    fix security group output elb

--- outputs.tf
+++ outputs.tf
@@ -55,7 +55,7 @@ output "target_group_names" {
 
 output "security_groups" {
   description = "List of Security Groups IDs"
-  value       = "${element(concat(aws_lb.application_no_logs.*.security_groups, aws_lb.network_no_logs.*.security_groups), 0)}"
+  value       = "${aws_lb.application_no_logs.*.security_groups}"
 }
 
 output "arn" {
